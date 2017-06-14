327427fc4113f1dfa7c43f39e43684e3a4879754
Vincent <cloudy.sonata@gmail.com>
2017-06-13 20:34:31 -0400

    star board

--- bot.js
+++ bot.js
@@ -3,6 +3,10 @@ const path = require('path');
 const sqlite = require('sqlite');
 const fs = require('fs');
 
+// All credits for starboard system to: https://github.com/WeebDev/Commando
+// This bot has a very simplified implementation of it
+const Starboard = require('./structures/starboard');
+
 // Initialise configuration file, create one if it doesn't exist
 if (!fs.existsSync('./config.json')) {
     fs.writeFileSync('./config.json', '{"discord_bot_token" : "", "command_prefix" : "-"}');
@@ -33,7 +37,22 @@ client
     .on('commandError', (cmd, err) => {
         if(err instanceof Commando.FriendlyError) return;
         console.error('Error in command ${cmd.groupID}:${cmd.memberName}', err)
-    });
+    })
+    .on('message', () => {
+        // Do nothing for now, but necessary to fire 'messageReactionAdd' events
+    })
+    // Initialize #starboard features
+    .on('messageReactionAdd', (reaction, user) => {
+		if (reaction.emoji.name !== '⭐') {
+            return;
+        }
+		const { message } = reaction;
+		const starboard = message.guild.channels.find('name', 'starboard');
+		if (!starboard) {
+            return message.channel.send(`${user}, can't star things without a #starboard.`); // eslint-disable-line consistent-return, max-len
+        }
+        Starboard.createStar(message, starboard, user.id);
+	});
     
 // Database
 client.setProvider(
---
+++ structures/starboard.js
@@ -0,0 +1,74 @@
+// All credits for starboard system to: https://github.com/WeebDev/Commando
+const path = require('path');
+const { URL } = require('url');
+
+module.exports = class Starboard {
+	static async createStar(message, starboardChannel, starBy) {
+		message.content = message.content.length <= 1024 ? message.content : `${message.content.substr(0, 1021)}...`;
+		const starboardMessage = await starboardChannel.send({ embed: Starboard._starEmbed(message) });
+	}
+    
+	static _starEmbed(message) {
+		let attachmentImage;
+		const extensions = new Set(['.png', '.jpg', '.jpeg', '.gif', '.webp']);
+		const linkRegex = /https?:\/\/(?:\w+\.)?[\w-]+\.[\w]{2,3}(?:\/[\w-_\.]+)+\.(?:png|jpg|jpeg|gif|webp)/; // eslint-disable-line no-useless-escape, max-len
+
+		if (message.attachments.some(attachment => {
+			try {
+				const url = new URL(attachment.url);
+				const ext = path.extname(url.pathname);
+				return extensions.has(ext);
+			} catch (error) {
+				if (error.message !== 'Invalid URL') {
+                    console.log(error);
+                }
+
+				return false;
+			}
+		})) attachmentImage = message.attachments.first().url;
+
+		if (!attachmentImage) {
+			const linkMatch = message.content.match(linkRegex);
+			if (linkMatch) {
+				try {
+					const url = new URL(linkMatch[0]);
+					const ext = path.extname(url.pathname);
+					if (extensions.has(ext)) {
+                        attachmentImage = linkMatch[0]; // eslint-disable-line max-depth
+                    } 
+				} catch (error) {
+					if (error.message === 'Invalid URL') {
+                        console.log('ERROR: No valid image link.'); // eslint-disable-line max-depth
+                    }
+					console.log(error);
+				}
+			}
+		}
+
+		return {
+			author: {
+				icon_url: message.author.displayAvatarURL, // eslint-disable-line camelcase
+				name: `${message.author.username}#${message.author.discriminator} (${message.author.id})`
+			},
+			color: 0xFFAC33,
+			fields: [
+				{
+					name: 'ID',
+					value: message.id,
+					inline: true
+				},
+				{
+					name: 'Channel',
+					value: message.channel.toString(),
+					inline: true
+				},
+				{
+					name: 'Message',
+					value: message.content ? message.content : '\u200B'
+				}
+			],
+			image: { url: attachmentImage || undefined },
+			timestamp: message.createdAt
+		};
+	}
+};
