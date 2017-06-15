ed767978f7940b081403cdc4c7360cb15c804806
Vincent <cloudy.sonata@gmail.com>
2017-06-15 16:37:20 -0400

    Switch to chrono for timezone support

--- commands/general/remindme.js
+++ commands/general/remindme.js
@@ -1,46 +1,56 @@
 const { Command } = require('discord.js-commando');
 const moment = require('moment');
-const sherlock = require('sherlockjs');
+const chrono = require('chrono-node');
 const { stripIndents } = require('common-tags');
 
-// Full credits to: https://github.com/WeebDev/Commando/
+// Credits to: https://github.com/WeebDev/Commando/
 module.exports = class RemindMeCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'remindme',
-			aliases: ['remind'],
-			group: 'util',
+			group: 'general',
 			memberName: 'remindme',
 			description: 'Reminds you of something.',
+            examples: ["remindme Appointment in 2 hours"],
 			guildOnly: true,
+            
 			args: [
 				{
-					key: 'remind',
-					label: 'reminder',
-					prompt: 'what would you like me to remind you about?\n',
+					key: 'text',
+					label: 'text',
+					prompt: 'What would you like me to remind you about?',
 					type: 'string',
-					validate: time => {
-						const remindTime = sherlock.parse(time);
-						if (!remindTime.startDate) {
+					validate: text => {
+						const parsedTime = chrono.parseDate(text);
+						if (!parsedTime) {
                             return `please provide a valid starting time.`;
                         }
 						return true;
-					},
-					parse: time => sherlock.parse(time)
+					}
 				}
 			]
 		});
 	}
     
-	async run(message, { remind }) {
-		const time = remind.startDate.getTime() - Date.now();
-		const preRemind = await message.say(stripIndents`
-			${message.author}, I will remind you about \`${remind.eventTitle}\` ${moment().add(time, 'ms').fromNow()}.`);
-		const remindMessage = await new Promise(resolve => {
+	async run(message, { text }) {
+        // Parse text into chrono date
+        const parsedDate = chrono.parse(text);
+        
+        // Get relevant results from chrono date object
+        const dateText = parsedDate[0].text; // The text parsed to get date (e.g. "at 4pm tomorrow")
+        const dateTime = parsedDate[0].start.date(); // DateTime object parsed
+        
+        // Get event title by removing date-related text from text given
+        var eventTitle = text.replace(dateText, '');
+        
+		const time = dateTime.getTime() - Date.now();
+		const remindInitiate = await message.say(stripIndents`
+			${message.author}, I will remind you about \`${eventTitle}\` ${moment().add(time, 'ms').fromNow()}.`);
+		const remindResult = await new Promise(resolve => {
 			setTimeout(() => resolve(message.say(stripIndents`
-				${message.author}, you wanted me to remind you of: \`${remind.eventTitle}\``)), time);
+				${message.author}, you wanted me to remind you of: \`${eventTitle}\``)), time);
 		});
 
-		return [preRemind, remindMessage];
+		return [remindInitiate, remindResult];
 	}
 };
